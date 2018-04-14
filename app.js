d4231b622375862cbe2d33602453deff7f5b65a6
Movses Saponjyan <movses.saponjyan@iunetworks.am>
2018-04-14 16:56:57 +0400

    Change

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'bin')));
 
 
 app.use('/v1/api', indexRouter);
--- public/index.js
+++ public/index.js
@@ -1,3 +1,71 @@
 /**
  * Created by movsessaponjyan on 4/14/18.
  */
+
+
+angular
+    .module("app",[])
+    .controller("mainCtrl",function ($scope) {
+        $scope.test = "Hello";
+
+        $scope.selectAnswer = null;
+
+        $scope.changePages = function () {
+            $scope.homePage = false;
+            $scope.rulesPage = false;
+            $scope.votePage = false;
+        };
+        $scope.changePages($scope.homePage);
+
+        $scope.homePage = true;
+
+        $scope.guessPageAction = function () {
+            $scope.changePages();
+            $scope.rulesPage = true;
+        };
+
+        $scope.setAnswer = function (answer) {
+            $scope.selectAnswer = answer;
+        };
+
+        $scope.approveAnswer = function () {
+
+            if ($scope.selectAnswer != null){
+                console.log($scope.selectAnswer);
+
+                $scope.selectAnswer = null;
+                $scope.answers = [{
+                    id: 1,
+                    label: 'QUEST 2'
+                }, {
+                    id: 2,
+                    label: 'Quest 2'
+                }];
+            } else {
+                alert("Choose");
+            }
+
+
+        };
+        
+        $scope.votePageAction = function () {
+            $scope.changePages();
+            $scope.votePage = true;
+
+
+            $scope.answers = [{
+                id: 1,
+                label: 'aLabel'
+            }, {
+                id: 2,
+                label: 'bLabel'
+            }];
+
+        };
+
+        // $scope.votePageAction(); //tmp
+
+
+
+    });
+
